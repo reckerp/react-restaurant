874121ec16e541970d29b4280e05c25da69c68a9
reckerp <github@paulrecker.de>
2023-03-18 21:57:53 +0100

    feat: add header implementation

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
+import {images} from '../../constants'
 import './Header.css';
+import {SubHeading} from "../../components";
 
 const Header = () => (
-  <div>
-    Header
-  </div>
+    <div className="app__header app__wrapper section__padding" id="home">
+        <div className="app__wrapper_info">
+            <SubHeading title={"Chase the new flavour"}/>
+            <h1 className="app__header-h1">The Key to Fine Dining</h1>
+            <p className="p__opensans" style={{margin:'2rem 0'}}>Nulla dolor in velit dolore qui. Aliquip velit labore veniam officia elit reprehenderit ex velit nostrud. Quis cupidatat ipsum cupidatat aliquip eu ea eiusmod sunt dolore.</p>
+            <button className="custom__button" type="button">Explore Menu</button>
+        </div>
+        <div className="app__wrapper_img">
+            <img src={images.welcome} alt="welcome"/>
+        </div>
+    </div>
 );
 
 export default Header;
