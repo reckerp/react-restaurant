4f24445235b2e1d4e42f23489147dafe42a22f60
reckerp <github@paulrecker.de>
2023-03-18 22:01:07 +0100

    style: format code

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,7 +9,9 @@ const Header = () => (
         <div className="app__wrapper_info">
             <SubHeading title={"Chase the new flavour"}/>
             <h1 className="app__header-h1">The Key to Fine Dining</h1>
-            <p className="p__opensans" style={{margin:'2rem 0'}}>Nulla dolor in velit dolore qui. Aliquip velit labore veniam officia elit reprehenderit ex velit nostrud. Quis cupidatat ipsum cupidatat aliquip eu ea eiusmod sunt dolore.</p>
+            <p className="p__opensans" style={{margin: '2rem 0'}}>Nulla dolor in velit dolore qui. Aliquip velit labore
+                veniam officia elit reprehenderit ex velit nostrud. Quis cupidatat ipsum cupidatat aliquip eu ea eiusmod
+                sunt dolore.</p>
             <button className="custom__button" type="button">Explore Menu</button>
         </div>
         <div className="app__wrapper_img">
